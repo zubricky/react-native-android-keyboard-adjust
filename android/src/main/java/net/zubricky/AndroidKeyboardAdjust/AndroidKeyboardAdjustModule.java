d2c5d57a90c307eff82f54bbb04c606f2c233589
jreziga <jessym.reziga@gmail.com>
2016-09-13 19:53:01 +0200

    Compatibility react 0.29.0

--- android/src/main/java/net/zubricky/AndroidKeyboardAdjust/AndroidKeyboardAdjustModule.java
+++ android/src/main/java/net/zubricky/AndroidKeyboardAdjust/AndroidKeyboardAdjustModule.java
@@ -12,11 +12,8 @@ import com.facebook.react.bridge.ReactMethod;
  */
 public class AndroidKeyboardAdjustModule extends ReactContextBaseJavaModule {
 
-    private Activity activity;
-
     public AndroidKeyboardAdjustModule(ReactApplicationContext reactApplicationContext) {
         super(reactApplicationContext);
-        this.activity = getCurrentActivity();
     }
 
     @Override
@@ -26,90 +23,145 @@ public class AndroidKeyboardAdjustModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void setAdjustNothing() {
-        this.activity.runOnUiThread(new Runnable() {
+
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+        
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING);
             }
         });
     }
 
     @ReactMethod
     public void setAdjustPan() {
-        this.activity.runOnUiThread(new Runnable() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
             }
         });
     }
 
     @ReactMethod
     public void setAdjustResize() {
-        this.activity.runOnUiThread(new Runnable() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
             }
         });
     }
 
     @ReactMethod
     public void setAdjustUnspecified() {
-        this.activity.runOnUiThread(new Runnable() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_UNSPECIFIED);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_UNSPECIFIED);
             }
         });
     }
 
     @ReactMethod
     public void setAlwaysHidden() {
-        this.activity.runOnUiThread(new Runnable() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
             }
         });
     }
 
     @ReactMethod
     public void setAlwaysVisible() {
-        this.activity.runOnUiThread(new Runnable() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
             }
         });
     }
 
     @ReactMethod
     public void setVisible() {
-        this.activity.runOnUiThread(new Runnable() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
             }
         });
     }
 
     @ReactMethod
     public void setHidden() {
-        this.activity.runOnUiThread(new Runnable() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
             }
         });
     }
 
     @ReactMethod
     public void setUnchanged() {
-        this.activity.runOnUiThread(new Runnable() {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            return;
+        }
+
+        activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AndroidKeyboardAdjustModule.this.activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED);
+                activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_UNCHANGED);
             }
         });
     }
